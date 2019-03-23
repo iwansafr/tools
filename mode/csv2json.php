d9816f7daedf88a15950dd4a0af1772244c5f1ff
iwansafr <iwansafr@gmail.com>
2019-03-23 08:27:33 +0700

    bugfix csv to json

--- mode/csv2json.php
+++ mode/csv2json.php
@@ -2,13 +2,19 @@
 if(!empty($_POST))
 {
 	echo '<div class="output" style="padding: 10px; margin-top: 10px;background: black; color: white;">';
-		$a = explode(',',$_POST['code']);
+		$b = $_POST['code'];
+		$b = ','.$b;
+		$b = preg_replace('~[\n]~', ',', $b);
+		$b = str_replace(',,', ',', $b);
+		$a = explode(',',$b);
 		$number = 0;
 		$title = array();
 		foreach ($a as $key => $value) 
 		{
 			if(preg_match('~[\r\n]~',$value))
 			{
+				$value = explode("\n", $value);
+				$value = $value[0];
 				$value = preg_replace('~[\r\n]~', '', $value);
 				$value = str_replace("'", '', $value);
 				unset($a[$key]);
@@ -23,13 +29,13 @@ if(!empty($_POST))
 			unset($a[$key]);
 			$number++;
 		}
-
 		$data = array();
 		$i = 0;
 		$j = 0;
 		$new_a = array();
 		foreach ($a as $key => $value) 
 		{
+			$value = str_replace("\r", '', $value);
 			$new_a[$key] = str_replace("'",'',$value);
 		}
 		foreach ($new_a as $key => $value) 
@@ -42,6 +48,6 @@ if(!empty($_POST))
 			}
 		}
 		$data = json_encode($data);
-		pr($data);
+		echo $data;
 	echo '</div>';
 }
